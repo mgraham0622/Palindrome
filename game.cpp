a29a4838a4a25c17dcdb67ef2ac3c2574d644381
mgraham0622 <mmgraham0622@gmail.com>
2026-09-03 09:03:27 -0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -8,12 +8,15 @@ using namespace std;
 int strip(char* input){
   char temp[81];
   int length;
+  char ch1;
+  unsigned char ch2;
   length = strlen(input);
   for (int i < length, i++){
-    if(isalnum(str[i])){
-      char ch1 = str[i];
-      unsigned char ch2 = static_cast<unsigned char>(ch1);
+    ch1 = str[i];
+    ch2 = static_cast<unsigned char>(ch1);
+    if(isalnum(ch2)){
       ch2 = tolower(ch2));
+      ch1 = static_cast<char>(ch2);
     }
   }
   strcpy(temp,input);
