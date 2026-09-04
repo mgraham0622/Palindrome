ff0acdda281fa8420ef55784d0e681b29579f6ce
mgraham0622 <mmgraham0622@gmail.com>
2026-09-04 15:08:48 -0700

    All my changes from class couldn't push because of a mismatch
    I just redid them from memory in the GitHub terminal this probably doesn't work

--- game.cpp
+++ game.cpp
@@ -5,34 +5,53 @@
 
 using namespace std;
 
-int strip(char* input){
-  char temp[81];
+int strip(char (& input)[81]){
+  char temp[81] = "";
   int length;
   char ch1;
   unsigned char ch2;
   length = strlen(input);
-  for (int i < length, i++){
-    ch1 = str[i];
+  for (int i; i < length; i++){
+    ch1 = input[i];
     ch2 = static_cast<unsigned char>(ch1);
     if(isalnum(ch2)){
       ch2 = tolower(ch2));
       ch1 = static_cast<char>(ch2);
-      strcat(temp,ch1);
+      strncat(temp,&ch1,1);
     }
   }
-  strcpy(temp,input);
+  strcpy(input,temp);
   return 0;
 }
 
-int reverse(){
+int reverse(char (& input)[81]){
+  char temp[81] = "";
+  int length;
+  
+  length = strlen(input);
+  for (int i; i <= length; i++){
+      strncat(temp,&(input[length-i]),1);
+    }
+  }
+  strcpy(input,temp);
   return 0;
 }
+
 
 int main(){
   char input[81];
+  char reversed[81];
+  char dialogue1[11] = "palindrome";
+  char dialogue2[17] = "not a palindrome";
   cin.get(input,81);
-  cin.ignore(9999999,"\n");
+  cin.ignore(9999999,'\n');
   strip(input);
-  cout << input;
+  strcpy(reversed,input);
+  reverse(reversed);
+  if (strcmp(input,reverse) == 0){
+    cout << dialogue1 << endl;
+  } else {
+    cout << dialogue2 << endl;
+  }
   return 0;
 }
